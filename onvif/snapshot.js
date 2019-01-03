cd2b960d9fb3f84cd48626248a3d09b0edc8bb47
zerberros <cerverros@gmail.com>
2019-01-03 07:26:57 +0100

    I have removed the active / deactivated state and removed the interval function.

--- onvif/snapshot.js
+++ onvif/snapshot.js
@@ -4,25 +4,29 @@ module.exports = (RED) => {
 
     function snapshot(config) {
         RED.nodes.createNode(this, config);
-        this.active = config.active;
+        //this.active = config.active;
         var node = this;
 
         if (!config.url) {
             node.warn("No URL is specified. Please specify in node configuration.");
             return;
         }
-        if (node.active == false) return;
+        //if (node.active == false) return;
 
-        config.interval = parseInt(config.interval);
-        node.intervalId = null;
+        //config.interval = parseInt(config.interval);
+        //node.intervalId = null;
 
-        runInterval(node, config);
+        // runInterval(node, config);
 
-        node.on("close", () => {
-            if (this.intervalId != null) {
-                clearInterval(this.intervalId);
-            }
-        });
+        //node.on("close", () => {
+        //    if (this.intervalId != null) {
+        //        clearInterval(this.intervalId);
+        //    }
+        //});
+
+	node.on('input',() =>{	
+            takesnap(node, config);
+	});
     }
     RED.nodes.registerType("ONVIF Snapshot", snapshot);
 
@@ -30,21 +34,22 @@ module.exports = (RED) => {
         var node = RED.nodes.getNode(req.params.id);
         var state = req.params.state;
         if (node !== null && typeof node !== "undefined" ) {
-            if (state === "enable") {
-                node.active = true;
-                res.sendStatus(200);
-            } else if (state === "disable") {
-                node.active = false;
-                res.sendStatus(201);
-            } else {
-                res.sendStatus(404);
-            }
+        //    if (state === "enable") {
+        //        node.active = true;
+              res.sendStatus(200);
+        //    } else if (state === "disable") {
+        //        node.active = false;
+        //        res.sendStatus(201);
+        //    } else {
+        //        res.sendStatus(404);
+        //    }
         } else {
             res.sendStatus(404);
         }
     });
 
-    function runInterval(node, config) {
+
+/*    function runInterval(node, config) {
         if (node.intervalId != null) {
             clearInterval(node.intervalId);
         }
@@ -82,5 +87,39 @@ module.exports = (RED) => {
         node.intervalId = setInterval(() => {
             fetch();
         }, config.interval * 1000);
+    } */
+
+
+    function takesnap(node, config) {
+ 
+        let msg = {
+            name: config.name,
+            url: config.url,
+            error: false
+        };
+
+        let fetch = function() {
+            let onvifInstance = new onvif.OnvifDevice({
+                xaddr: config.url,
+                user : config.username,
+                pass : config.password
+            });
+
+            onvifInstance.init().then((info) => {
+                node.log('Fetching snapshot from ' + config.url);
+                return onvifInstance.fetchSnapshot();
+            }).then((res) => {
+                let prefix = 'data:' + res.headers['content-type'] + ';base64,';
+                let base64Image = Buffer.from(res.body, 'binary').toString('base64');
+                msg.payload = prefix + base64Image;
+                msg.binaryImage = res.body;
+                node.send(msg);
+            }).catch((error) =>{
+	        msg.payload = null;
+		msg.error = error;
+		node.send(msg);
+	    });
+        }
+	fetch();
     }
 }

0d2175fdc78a1668cf2c71511833e429aeb0510d
Razvan Stanga <git@razvi.ro>
2020-11-11 18:20:24 +0200

    added output image resize

--- onvif/snapshot.js
+++ onvif/snapshot.js
@@ -1,48 +1,32 @@
 module.exports = (RED) => {
     "use strict";
     let onvif = require("node-onvif");
+    const sharp = require('sharp');
 
     function snapshot(config) {
         RED.nodes.createNode(this, config);
         let node = this;
 
         node.on("input", function (msg) {
-            try {
-                let _msg = JSON.parse(msg.payload);
-                if (typeof _msg === "object") {
-                    if(_msg.hasOwnProperty("name")) {
-                        msg.name = _msg.name;
-                    }
-                    if(_msg.hasOwnProperty("url")) {
-                        msg.url = _msg.url;
-                    }
-                    if(_msg.hasOwnProperty("username")) {
-                        msg.username = _msg.username;
-                    }
-                    if(_msg.hasOwnProperty("password")) {
-                        msg.password = _msg.password;
-                    }
-                }
-            }
-            catch (ex) {}
 
-            config.name     = msg.name || config.name;
-            config.url      = msg.url || config.url;
-            config.username = msg.username || config.username;
-            config.password = msg.password || config.password;
+            config.name     = msg.payload.name      || config.name;
+            config.url      = msg.payload.url       || config.url;
+            config.username = msg.payload.username  || config.username;
+            config.password = msg.payload.password  || config.password;
+            config.resize   = msg.payload.resize    || config.resize;
 
             if(msg.hasOwnProperty("payload")) {
                 msg._payload = msg.payload;
             }
             msg.node = this.type;
 
+            if (!config.url) {
+                node.warn("No URL is specified. Please specify in node configuration.");
+                return;
+            }
+
             run(msg, node, config);
         });
-
-        if (!config.url) {
-            node.warn("No URL is specified. Please specify in node configuration.");
-            return;
-        }
     }
     RED.nodes.registerType("ONVIF Snapshot", snapshot);
 
@@ -58,15 +42,35 @@ module.exports = (RED) => {
             return onvifInstance.fetchSnapshot();
         }).then((res) => {
             let prefix = 'data:' + res.headers['content-type'] + ';base64,';
-            let base64Image = Buffer.from(res.body, 'binary').toString('base64');
-            msg.payload = {
-                config: config,
-                image: {
-                    base64: (prefix + base64Image),
-                    binary: res.body
-                }
-            };
-            node.send(msg);
+
+            if (config.resize) {
+                sharp(Buffer.from(res.body, 'binary'))
+                    .resize(config.resize)
+                    .toFormat('png')
+                    .toBuffer()
+                    .then( data => {
+                        msg.payload = {
+                            config: config,
+                            image: {
+                                base64: (prefix + data.toString('base64')),
+                                binary: res.body
+                            }
+                        };
+                        node.send(msg);
+                    }).catch( err => {
+
+                    });
+            } else {
+                let base64Image = Buffer.from(res.body, 'binary').toString('base64');
+                msg.payload = {
+                    config: config,
+                    image: {
+                        base64: (prefix + base64Image),
+                        binary: res.body
+                    }
+                };
+                node.send(msg);
+            }
         }).catch((error) => {
             msg.payload = null;
             msg.error = error;
